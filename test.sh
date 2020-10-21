77184204249710c9434e674c5d9c8ad8f2ae8981
YBA-IBM <47538942+YBA-IBM@users.noreply.github.com>
2020-10-21 19:04:48 -0400

    Fix elasticsearch versions
    Signed-off-by: YBA-IBM <yoel.ben-avraham@ibm.com>

--- test.sh
+++ test.sh
@@ -10,8 +10,8 @@ echo -e "\n $ANSI_YELLOW *** FUNCTIONAL TEST(S) *** $ANSI_RESET \n"
 
 echo -e "$ANSI_YELLOW It can run a Java program: $ANSI_RESET"
 docker network create somenetwork
-docker run --rm -d --name elasticsearch --net somenetwork -p 9200:9200 -p 9300:9300 -e "discovery.type=single-node" elasticsearch:7.9.2
-docker run --rm -p 9200:9200 -p 9300:9300 -e "discovery.type=single-node" docker.elastic.co/elasticsearch/elasticsearch:7.9.2
+docker run --rm -d --name elasticsearch --net somenetwork -p 9200:9200 -p 9300:9300 -e "discovery.type=single-node" quay.io/ibmz/elasticsearch:7.9.2
+docker run --rm -p 9200:9200 -p 9300:9300 -e "discovery.type=single-node" quay.io/ibmz/elasticsearch:7.9.2
 docker network rm somenetwork
 
 
