7146c96fac5e6229c863a32feb0e6d7cfe0af067
YBA-IBM <47538942+YBA-IBM@users.noreply.github.com>
2020-10-21 19:32:24 -0400

    Change ports
    Signed-off-by: YBA-IBM <yoel.ben-avraham@ibm.com>

--- test.sh
+++ test.sh
@@ -10,7 +10,7 @@ echo -e "\n $ANSI_YELLOW *** FUNCTIONAL TEST(S) *** $ANSI_RESET \n"
 
 echo -e "$ANSI_YELLOW It can run a Java program: $ANSI_RESET"
 docker network create somenetwork
-docker run --rm -d --name elasticsearch --net somenetwork -p 9200:9200 -p 9300:9300 -e "discovery.type=single-node" quay.io/ibmz/elasticsearch:7.9.1
+docker run --rm -d --name elasticsearch --net somenetwork -p 9400:9400 -p 9500:9500 -e "discovery.type=single-node" quay.io/ibmz/elasticsearch:7.9.1
 docker run --rm -p 9200:9200 -p 9300:9300 -e "discovery.type=single-node" quay.io/ibmz/elasticsearch:7.9.1
 docker network rm somenetwork
 
